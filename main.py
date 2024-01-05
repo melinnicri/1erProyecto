33ee36c948c8f5cd3475d1602fb389f540a66579
melinnicri <123215286+melinnicri@users.noreply.github.com>
2024-01-05 19:56:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Query
 from fastapi.responses import HTMLResponse
 import pandas as pd
 import importlib
+import ParquetFile
 import numpy as np
 import pyarrow
 import fastparquet
